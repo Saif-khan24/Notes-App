e202300e53619f7c08253a9e3248240356ad34b0
Saif <kmohdsaif40@gmail.com>
2021-07-22 22:15:43 +0530

    changes in script tag

--- app.js
+++ app.js
@@ -87,3 +87,54 @@ search.addEventListener('input', function(){
         }
     });
 });
+
+
+/*
+let search = document.getElementById('searchTxt');
+search.addEventListener('input', function(){
+
+    let inputVal = search.value.toLowerCase();
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName('p')[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = 'block';
+        }
+        else{
+            element.style.display = 'none';
+        }
+    });
+
+    html += `
+        <div class="noteCard my-2 mx-2 card" style='width: 18rem;'>
+            <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class='card-text'>${element.text}</p>
+                <button id='${index}' onclick='deleteNote(this.id)' class='btn btn-primary'>Delete Note</button>
+            </div>
+          </div>
+        `;
+});
+function deleteNote(index){
+    // console.log("i am deleting ", index);
+
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+      } else {
+        n
+        html += `
+        <div class="noteCard my-2 mx-2 card" style='width: 18rem;'>
+            <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class='card-text'>${element.text}</p>
+                <button id='${index}' onclick='deleteNote(this.id)' class='btn btn-primary'>Delete Note</button>
+            </div>
+          </div>
+        `;otesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+}
+*/
